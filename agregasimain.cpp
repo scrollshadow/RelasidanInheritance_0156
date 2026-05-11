a104437ef87d10f702a55570070462a256a4b2dd
scrollshadow <zaldhypramana@gmail.com>
2026-05-11 15:22:47 +0700

    21

--- agregasimain.cpp
+++ agregasimain.cpp
@@ -8,7 +8,7 @@ int main() { //agre
     ibu* varIbu2 = new ibu("novi");
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("rini");
-    anak* varAnak3 = new anak("Dew");
+    anak* varAnak3 = new anak("Dewi");
 
     varIbu->tambahAnak(varAnak1);
     varIbu->tambahAnak(varAnak2);
