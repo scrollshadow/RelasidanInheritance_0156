a131ef96639a4ffcb0bf40f093336dd114173101
scrollshadow <zaldhypramana@gmail.com>
2026-05-11 15:10:58 +0700

    10

---
+++ jantung.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#ifndef JANTUNG_H
+#define JANTUNG_H
+using namespace std;
+
+class jantung {
+public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+#endif
